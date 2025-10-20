bea9d28fac8094181868a41d531175c15b6a38ed
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-10-20 21:21:08 +0900

    feat: 그 외 숫자 배열 합산 기능 구현

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,8 +1,12 @@
 export class Calculator {
     static sum(numbers) {
-        // numbers가 비어 있으면 0 반환
+        // 빈 배열인 경우 0 반환
         if (!numbers || numbers.length === 0){
             return 0;
         }
+
+        // 그 외 숫자 배열인 경우 모든 숫자 합산
+        const total = numbers.reduce((acc, cur) => acc + cur, 0);
+        return total;
     }
 }
