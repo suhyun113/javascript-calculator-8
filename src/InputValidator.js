bf5f82cf75a87f08472ed8e0a2d3510da4eb154f
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-10-20 21:14:58 +0900

    feat: 양수가 아닌 값인 경우 검증 기능 추가

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -6,5 +6,13 @@ export class InputValidator {
         if (hasNonNumber) {
             throw new Error("[ERROR] 숫자가 아닌 값이 포함되어 있습니다.");
         }
+
+        // 양수가 아닌 값(0 또는 음수)
+        const hasNegative = numbers.some((n) => n <= 0);
+        if (hasNegative) {
+            throw new Error("[ERROR] 양수가 아닌 값이 포함되어 있습니다.");
+        }
+
+        return true;
     }
 }
